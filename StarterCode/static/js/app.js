ad81a8b2e12c1f4b9247228c8dae743256500e57
macobby1 <60025531+macobby1@users.noreply.github.com>
2020-05-19 17:19:59 -0500

    adding changes

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -1,85 +1,174 @@
 
-function buildMetadata(sample) {
-    var url = `/metadata/${sample}`;
-    d3.json(url).then(function(response) {
-   
-    var sampleMetadata = d3.select("#sample-metadata");
-    sampleMetadata.html("");
-   
-    var data = Object.entries(response);
-    data.forEach(function(item) {
-    sampleData.append("div").text(item);
-   });
-   })}
-  
-  
-  function buildCharts(sample) {
-    var sampleData = `/samples/${sample}`;
-    
-    d3.json(sampleData).then(function(response){
-      var topTenOtuIds = response.otu_ids.slice(0,10);
-      var topOtuLabels = response.otu_labels.slice(0,10);
-      var topTenSampleValues = response.sample_values.slice(0,10);
-  
-      var data = [{
-        "labels": topTenOtuIds,
-        "values": topTenSampleValues,
-        "hovertext": topOtuLabels,
-        "type": "pie"
-      }];
-      
-    Plotly.newPlot('pie', data);
-  
-    d3.json(sampleData).then(function(response){
-      var bubbleOtuIds = response.otu_ids;
-      var bubbleOuLabels = response.otu_labels;
-      var bubbleSampleValues = response.sample_values;
-  
-      var bubbleChartData = {
-        mode: 'markers',
-        x: bubbleOtuIds,
-        y: bubbleSampleValues,
-        text: bubbleOuLabels,
-        marker: {color: bubbleOtuIds, colorscale: 'Rainbow', size: bubbleSampleValues}
-      };
-  
-      var bblData = [bubbleChartData];
-  
-      var layout = {
-        showlegend: false,
-        height: 600,
-        width: 1200
-        };
-      
-      Plotly.newPlot('bubble', bblData, layout); 
-    })
-  })
-  }
-  function init() {
-    // Grab a reference to the dropdown select element
-    var selector = d3.select("#selDataset");
-  
-    // Use the list of sample names to populate the select options
-    d3.json("/names").then((sampleNames) => {
-      sampleNames.forEach((sample) => {
-        selector
+function init(){
+  d3.json("samples.json").then((data) => {
+      // create a dropdown menu
+      var idList = data.names;
+      d3.select("#selDataset").selectAll("option")
+          .data(idList)
+          .enter()
           .append("option")
-          .text(sample)
-          .property("value", sample);
+          .attr("value", d=>d)
+          .text(d=>d);
+      
+      // make the first id the default page
+      const firstName = idLIst[0];
+      buildCharts(firstName);
+      demoInfo(firstName);
+          
+  });
+}
+
+function buildCharts(name){
+
+  d3.json("samples.json").then((data) => {
+
+      var ds = data.samples;
+
+      var filteredData_1 = ds.filter(row => row.id == name);
+      var labels = filteredData_1[0].otu_ids;
+      var values = filteredData_1[0].sample_values;
+      var texts = filteredData_1[0].otu_labels;
+
+      // create a bar chart
+      var trace1 = {
+          x: values.slice(0, 10).reverse(),
+          y: labels.slice(0, 10).map(otuID => `OTU ${otuID}`).reverse(),
+          text: texts.slice(0, 10),
+          type: "bar",
+          hoverinfo: "text",
+          orientation: "h"
+      };
+
+      var layout = {
+          title: `<b>Top Ten OTUs for ID ${name}</b>`,
+          margin: {t: 100, l:100},
+          width: 500,
+          height: 600
+      };
+
+      var data = [trace1];
+      Plotly.newPlot("bar", data, layout);
+
+      // create a bubble chart
+      var trace2 = {
+          x: labels,
+          y: values,
+          text: texts,
+          mode: 'markers',
+          hoverinfo: "text",
+          marker: {
+              size: values,
+              color: labels,
+              colorscale: "Electric"
+          }
+      };
+
+      var layout2 = {
+          title: `<b>Bacteria Cultures for ID ${name} </b>`,
+          xaxis: { title: `OTU ID`},
+          yaxis: { title: "OTU Sample Value"},
+          width: 1250,
+          height: 500
+      };
+
+      var data2 = [trace2];
+      Plotly.newPlot("bubble", data2, layout2)
+  });
+};
+
+
+// Demographic info
+function demographicInfo(name){
+
+  d3.json("samples.json").then((data) => {
+
+      var md = data.metadata;
+
+      var filteredData = md.filter(row => row.id == name);
+      var filteredValue = filteredData[0];
+
+      var panelData = d3.select("#sample-metadata");
+      
+      panelData.html("");
+
+      Object.entries(filteredValue).forEach(([key, value]) => {
+          //Capitalize first letter
+          var newKey = key.charAt(0).toUpperCase() + key.slice(1);
+          //update info
+          panelData.append("p").text(`${newKey}: ${value}`);
       });
+
+      // create a Gauge Chart
+      var trace3 = {
+          type: "pie",
+          showlegend: false,
+          hole: 0.5,
+          rotation: 90,
+          values: [100 / 9, 100 / 9, 100 / 9, 100 / 9, 100 / 9, 100 /9, 100 / 9, 100 / 9, 100 / 9, 100],
+          text: ["0-1", "1-2", "2-3", "3-4", "4-5", "5-6", "6-7", "7-8", "8-9", ""],
+          direction: "clockwise",
+          textinfo: "text",
+          textposition: "inside",
+          marker: {
+            colors: ["rgba(241, 129, 48, 0.62)", "rgba(241, 164, 48, 0.62)", "rgba(241, 209, 48, 0.62)", "rgba(238, 241, 48, 0.62)", "rgba(216, 241, 48, 0.62)","rgba(180, 241, 48, 0.62)", "rgba(109, 241, 48, 0.62)", "rgba(48, 241, 64, 0.62)", "rgba(48, 241, 132, 0.62)",  "white"]
+          },
+          labels: ["0-1", "1-2", "2-3", "3-4", "4-5", "5-6", "6-7", "7-8", "8-9", ""],
+          hoverinfo: "text"
+        };
+
+      // create a pointer
+      var degrees = 180 - filteredValue.wfreq * 20, radius = .5;
+      var radians = degrees * Math.PI / 180;
+      var x = radius * Math.cos(radians);
+      var y = radius * Math.sin(radians);
+      var path1 = (degrees < 45 || degrees > 135) ? 'M -0.0 -0.025 L 0.0 0.025 L ' : 'M -0.025 -0.0 L 0.025 0.0 L ';
+      var mainPath = path1,
+          pathX = String(x),
+          space = ' ',
+          pathY = String(y),
+          pathEnd = ' Z';
+      var path = mainPath.concat(pathX, space, pathY, pathEnd);
+      
+      trace4 = {
+          type: 'scatter',
+          x:[0],
+          y:[0],
+          marker: {size: 14, color: '850000'},
+          showlegend: false,
+          text: `Washing frequency: ${filteredValue.wfreq}`,
+          hoverinfo: 'text'
+      };
+
+      var layout3 = {
+          width: 500,
+          height: 580,
+          margin: {t: 150},
+          shapes:[{
+              type: 'path',
+              path: path,
+              fillcolor: '850000',
+              line: {
+                  color: '850000'
+              }
+          }],
+          title: '<b>Belly Button Washing Frequency</b><br> Scrubs per Week',
+          xaxis: {visible: false, range: [-1, 1]},
+          yaxis: {visible: false, range: [-1, 1]}
+        };
+
   
-      // Use the first sample from the list to build the initial plots
-      const firstSample = sampleNames[0];
-      buildCharts(firstSample);
-      buildMetadata(firstSample);
-    });
-  }
-  
-  function optionChanged(newSample) {
-    // Fetch new data each time a new sample is selected
-    buildCharts(newSample);
-    buildMetadata(newSample);
-  }
-  
-  // Initialize the dashboard
-  init();
+      var data3 = [trace3, trace4];
+      Plotly.newPlot("gauge", data3, layout3);
+
+  });
+
+};
+
+//update data
+function optionChanged(newName){
+  buildCharts(newName);
+  demoInfo(newName);
+}
+
+// initialize the dashboard
+init();
